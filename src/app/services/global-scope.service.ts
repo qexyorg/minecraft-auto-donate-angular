3c8e39a5872db45106da65e88e167844af6e27a6
Qexy <admin@qexy.org>
2021-02-02 23:48:12 +0200

    First upload

---
+++ src/app/services/global-scope.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalScopeService {
+  public options: any = {};
+  public clients: number = 0;
+  public last: any = [];
+  public lastLoaded: boolean = false;
+
+  constructor() { }
+}
